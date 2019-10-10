892a63205a1b7ee1fad3ab28369f51bebf4d647f
Dao Tung <duytung95nb@gmail.com>
2019-10-10 23:13:23 +0700

    Basic auth to support client get access token

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,28 @@
 const router = require('express').Router();
 const passport = require('passport');
 const clients = require('../config/clients');
+const jwt = require('jsonwebtoken');
 
 router.get('/login', (req, res) => {
     var requestingClient = clients.find(client => client.clientID === req.query.client_id);
-    if(requestingClient === undefined) {
+    if (requestingClient === undefined) {
         res.status(401).send('Unauthorized client');
     }
+    if (req.user) {
+        const token = jwt.sign({
+            sub: req.user.id,
+            img: req.user.thumbnailUrl,
+            fullName: req.user.username
+        }, 'This is private key', { expiresIn: 3600 });
+        
+        res.render('login', {user: req.user,
+            accessToken: token,
+            clientRedirectUri: requestingClient.redirectUri
+        });
 
-    res.render('login', {user: req.user, clientRedirectUri: requestingClient.redirectUri});
+    } else {
+        res.render('login', {user: req.user, accessToken: '', clientRedirectUri: ''});
+    }
 });
 
 router.get('/logout', (req, res) => {
@@ -23,10 +37,10 @@ router.get('/google', passport.authenticate('google', {
 
 router.get('/google/redirect', passport.authenticate('google', { failureRedirect: 'auth/google' }),
     (req, res) => {
-    res.redirect('/auth/success');
-});
+        res.redirect('/auth/success');
+    });
 
 router.get('/success', (req, res) => {
-    res.render('authSuccess', {message: 'AuthSuccess'});
+    res.render('authSuccess', { message: 'AuthSuccess' });
 })
 module.exports = router;
